3dff0b7312703d593053e7da4bb7823508557530
lucas-maciel-afonso <lafonso@byupathway.edu>
2025-02-05 10:44:14 -0700

    test W04: Chamber Membership Join Page

---
+++ chamber/scripts/join.js
@@ -0,0 +1,21 @@
+// Get the current year
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("currentyear").innerHTML = year;
+
+// The date the document was last modified
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = "Last Modification: " + text;
+
+// Set the current timestamp in the hidden field
+document.getElementById('timestamp').value = new Date().toISOString();
+
+// Function to show modal
+function showModal(modalId) {
+    document.getElementById(modalId).style.display = 'block';
+}
+
+// Function to close modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+}
